88db4d43054afa1a32a6a631ba4cc4bc596d34fd
jungil <jungil.me@gmail.com>
2015-12-23 13:34:45 +0900

    remove GoogleMap

--- Library.GoogleMap/Events/IClientEventObject.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Library.GoogleMap
-{
-    public interface IClientEventObject
-    {
-        void SerializeTo(ClientSideObjectWriter writer);
-    }
-}
--- Library.GoogleMap/ISerializer.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Library.GoogleMap
-{
-    public interface ISerializer
-    {
-        IDictionary<string, object> Serialize();
-    }
-}
