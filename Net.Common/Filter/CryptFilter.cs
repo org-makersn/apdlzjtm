3d09a53c40d11587dd3b26639ae26aadbfc53412
jungil <jungil.me@gmail.com>
2016-01-19 10:49:21 +0900

    upd

--- Net.Common/Filter/CryptFilter.cs
+++ Net.Common/Filter/CryptFilter.cs
@@ -62,63 +62,24 @@ namespace Net.Common.Filter
             return cipherText;
         }
 
-        #region RSA암복호화
-        //RSA 암호화
-        public static string RSAEncrypt(string sValue, string sPubKey)
-        {
-            //공개키 생성
-            byte[] keybuf = Convert.FromBase64String(sPubKey);
-            sPubKey = (new UTF8Encoding()).GetString(keybuf);
-            RSACryptoServiceProvider oEnc = new RSACryptoServiceProvider(); //암호화
-
-            oEnc.FromXmlString(sPubKey);
-
-            //암호화할 문자열을 UFT8인코딩
-            byte[] inbuf = (new UTF8Encoding()).GetBytes(sValue);
-            //암호화
-            byte[] encbuf = oEnc.Encrypt(inbuf, false);
-
-            //암호화된 문자열 Base64인코딩
-            return Convert.ToBase64String(encbuf);
-        }
-
-        //RSA 복호화
-        public static string RSADecrypt(string sValue, string sPrvKey)
-        {
-            //개인키 생성
-            byte[] inbuf = Convert.FromBase64String(sPrvKey);
-            sPrvKey = (new UTF8Encoding()).GetString(inbuf);
-
-            //RSA객체생성
-            RSACryptoServiceProvider oDec = new RSACryptoServiceProvider(); //복호화
-            //개인키로 활성화
-            oDec.FromXmlString(sPrvKey);
-
-            //sValue문자열을 바이트배열로 변환
-            byte[] srcbuf = Convert.FromBase64String(sValue);
-
-            //바이트배열 복호화
-            byte[] decbuf = oDec.Decrypt(srcbuf, false);
-
-            //복호화 바이트배열을 문자열로 변환
-            string sDec = (new UTF8Encoding()).GetString(decbuf, 0, decbuf.Length);
-            return sDec;
-        }
-        #endregion
-
         #region DES암복호화
+        // Public Function
+        public static string DESEncrypt(string sValue)
+        {
+            return DesEncrypt(sValue, EncryptionKey);
+        }
 
         // Public Function
-        public static string DESEncrypt(string inStr)
+        public static string DESDecrypt(string sValue) // 복호화
         {
-            return DesEncrypt(inStr, EncryptionKey);
+            return DesDecrypt(sValue, EncryptionKey);
         }
 
         //문자열 암호화
-        private static string DesEncrypt(string str, string key)
+        private static string DesEncrypt(string sValue, string sPubKey)
         {
             //키 유효성 검사
-            byte[] btKey = ConvertStringToByteArrayA(key);
+            byte[] btKey = ConvertStringToByteArrayA(sPubKey);
 
             //키가 8Byte가 아니면 예외발생
             if (btKey.Length != 8)
@@ -127,7 +88,7 @@ namespace Net.Common.Filter
             }
 
             //소스 문자열
-            byte[] btSrc = ConvertStringToByteArray(str);
+            byte[] btSrc = ConvertStringToByteArray(sValue);
             DESCryptoServiceProvider des = new DESCryptoServiceProvider();
 
             des.Key = btKey;
@@ -149,17 +110,11 @@ namespace Net.Common.Filter
             return (ConvertByteArrayToStringB(btEncData));
         }//end of func DesEncrypt
 
-        // Public Function
-        public static string DESDecrypt(string inStr) // 복호화
-        {
-            return DesDecrypt(inStr, EncryptionKey);
-        }
-
         //문자열 복호화
-        private static string DesDecrypt(string str, string key)
+        private static string DesDecrypt(string sValue, string sPubKey)
         {
             //키 유효성 검사
-            byte[] btKey = ConvertStringToByteArrayA(key);
+            byte[] btKey = ConvertStringToByteArrayA(sPubKey);
 
             //키가 8Byte가 아니면 예외발생
             if (btKey.Length != 8)
@@ -168,7 +123,7 @@ namespace Net.Common.Filter
             }
 
 
-            byte[] btEncData = ConvertStringToByteArrayB(str);
+            byte[] btEncData = ConvertStringToByteArrayB(sValue);
             DESCryptoServiceProvider des = new DESCryptoServiceProvider();
 
             des.Key = btKey;
@@ -192,6 +147,8 @@ namespace Net.Common.Filter
 
         }
         //end of func DesDecrypt
+
+        #endregion //DES암복호화
 
         //문자열->유니코드 바이트 배열
         private static Byte[] ConvertStringToByteArray(String s)
@@ -228,7 +185,5 @@ namespace Net.Common.Filter
         {
             return Convert.ToBase64String(b);
         }
-
-        #endregion //DES암복호화
     }
 }
