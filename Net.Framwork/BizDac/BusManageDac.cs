e3c8d54af7520620a019994821f2063cd1da9ceb
azc2004 <azc2004@gmail.com>
2016-02-22 18:17:43 +0900

    add entity

--- Net.Framwork/BizDac/BusManageDac.cs
+++ Net.Framwork/BizDac/BusManageDac.cs
@@ -1,5 +1,5 @@
 ﻿using Net.Framework.Helper;
-using Net.Framework.StoreModel;
+using Net.Framework.Entity;
 using Net.SqlTools;
 using System;
 using System.Collections.Generic;
@@ -11,5 +11,40 @@ namespace Net.Framework.BizDac
 {
     public class BusManageDac : DacBase
     {
+        private ISimpleRepository<BusApplySchool> _makerbusApplyRepo = new SimpleRepository<BusApplySchool>();
+
+        #region AddApplyMakerbus - 메이커버스 신청서 저장
+        /// <summary>
+        /// 메이커버스 신청서 저장
+        /// </summary>
+        /// <param name="banner"></param>
+        /// <returns></returns>
+        internal int AddApplyMakerbus(BusApplySchool data)
+        {
+            int identity = 0;
+            bool ret = _makerbusApplyRepo.Insert(data);
+
+            if (ret)
+            {
+                identity = data.NO;
+            }
+            return identity;
+        }
+        #endregion
+
+        #region GetApplyMakerbusList - 메이커버스 전체 리스트
+        /// <summary>
+        /// 메이커버스 전체 리스트
+        /// </summary>
+        /// <returns></returns>
+        internal List<BusApplySchool> GetMakerbusList()
+        {
+            IEnumerable<BusApplySchool> state = new List<BusApplySchool>();
+
+            state = _makerbusApplyRepo.GetAll();
+
+            return state.ToList();
+        }
+        #endregion
     }
 }
--- Net.Framwork/Entity/BusManageModel.cs
+++ Net.Framwork/Entity/BusManageModel.cs
@@ -10,8 +10,83 @@ namespace Net.Framework.Entity
     }
 
     [Table("BUS_APPLY_SCHOOL")]
-    public class BUS_APPLY_SCHOOL
+    public class BusApplySchool
     {
- 
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int NO { get; set; }
+
+        public string SCHOOL_NAME { get; set; }
+        public string SCHOOL_ADDR { get; set; }
+        public string MANAGER { get; set; }
+        public string MANAGER_EMAIL { get; set; }
+        public string MANAGER_TEL { get; set; }
+        public string APPLY_PATH { get; set; }
+        public string MAKERBUS_YN { get; set; }
+        public Nullable<int> PARTICIPATION_COUNT { get; set; }
+        public Nullable<int> MODELING_COUNT { get; set; }
+        public Nullable<int> SUPPORT_PRINTER_COUNT { get; set; }
+        public Nullable<DateTime> EVENT_DATE { get; set; }
+        public string START_TIME { get; set; }
+        public string MEMO { get; set; }
+        public DateTime REG_DT { get; set; }
+        public string REG_ID { get; set; }
+        public Nullable<DateTime> CHG_DT { get; set; }
+        public string CHG_ID { get; set; }
     }
+
+    [Table("BUS_QNA")]
+    public class BusQna
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int NO { get; set; }
+        
+        public string CATEGORY { get; set; }
+        public string EMAIL { get; set; }
+        public string TITLE { get; set; }
+        public string CONTENTS { get; set; }
+        public DateTime REG_DT { get; set; }
+        public string REG_ID { get; set; }
+    }
+
+    [Table("BUS_FAQ")]
+    public class BusFaq
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int NO { get; set; }
+
+        public string TITLE { get; set; }
+        public string CONTENTS { get; set; }
+        public DateTime REG_DT { get; set; }
+        public string REG_ID { get; set; }
+    }
+
+    [Table("BUS_PARTNERSHIP_QNA")]
+    public class BusPartnershipQna
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int NO { get; set; }
+
+        public string TITLE { get; set; }
+        public string CONTENTS { get; set; }
+        public DateTime REG_DT { get; set; }
+        public string REG_ID { get; set; }
+    }
+
+    [Table("BUS_PARTNERSHIP_FAQ")]
+    public class BusPartnershipFaq
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int SEQ { get; set; }
+
+        public string TITLE { get; set; }
+        public string CONTENTS { get; set; }
+        public DateTime REG_DT { get; set; }
+        public string REG_ID { get; set; }
+    }
+
 }
