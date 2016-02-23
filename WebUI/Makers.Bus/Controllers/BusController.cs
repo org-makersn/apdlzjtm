29b243e0cafb3a7750e27ce2e6f05c7a5ff6f0ca
azc2004 <azc2004@gmail.com>
2016-02-23 11:11:46 +0900

    design update2

---
+++ WebUI/Makers.Bus/Controllers/BusController.cs
@@ -0,0 +1,41 @@
+﻿using Net.Common.Filter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Makers.Bus.Controllers
+{
+    public class BusController : BaseController
+    {
+        //
+        // GET: /Bus/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Apply()
+        {
+            return View();
+        }
+
+        public ActionResult Qna()
+        {
+            return View();
+        }
+
+        public ActionResult Faq()
+        {
+            return View();
+        }
+
+        public PartialViewResult SubHeader()
+        {
+            return PartialView();
+        }
+
+    }
+}
